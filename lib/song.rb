b89d385a4c4ea4cb2c28acd07ccbabbc6145bfe5
K Hong <albtrossfacefall@gmail.com>
2019-08-22 14:17:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
   
   def self.create_by_name(name_string)
     create.name=name_string
-    self.all.find{|song|song.name==name_string}
+    self.find_by_name(name_string)
   end
 
   def self.find_by_name(name_string)
