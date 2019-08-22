32112807b2066857793b80f8fe8d035362c65ca5
K Hong <albtrossfacefall@gmail.com>
2019-08-22 15:21:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,11 +33,10 @@ class Song
 
   def self.find_or_create_by_name(name_string)
     if self.find_by_name(name_string)
-      self.find_by_name(na)
+      self.find_by_name(name_string)
     else
-     
+      self.create_by_name(name_string)
     end
-    
   end
 
 end
